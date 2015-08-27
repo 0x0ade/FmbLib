d8c3c57b36b236a9fb25acd0c03b54de5c9f3c72
AngelDE98 <nuda1998@gmail.com>
2015-08-27 23:11:56 +0200

    Remove libs/, start working on FezSlim and XnaSlim

---
+++ XnaSlim/README.md
@@ -0,0 +1,7 @@
+Add all types you need here as slim, stripped skeletons.
+
+That means:
+
+* Avoid implementing methods, if required use stubs (f.e. OnDeserialization)
+* Only implement constructors, fields and properties
+* Keep the property behavior default (no external method calls on sets)
--- libs/dogfood_patch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cp ~/Spiele/FEZ/{FezEngine.orig.dll,ContentSerialization.dll,MonoGame.Framework.dll,SDL2-CS.dll} ./; mv FezEngine.orig.dll FezEngine.dll
-for file in *.dll; do
-  mono --debug ../FmbLibStripper/bin/Debug/FmbLibStripper.exe $file
-done
-#cp ~/Spiele/FEZOLD/{MonoGame.Framework.dll,SDL2#.dll} ./
-
